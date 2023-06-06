ea0695c6610a49a31e41c832ed044c6317e21ee4
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2023-06-06 11:12:02 -0400

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -14,12 +14,18 @@ public class GamePanel extends  JPanel implements Runnable{
     final int screenWidth = tileSize*maxScreenCol;  //768 pixels
     final int screenHeight = tileSize*maxScreenRow; //576 pixels
 
+    //FPS
+    int FPS = 60;
+
     KeyHandler keyH = new KeyHandler();
 
     Thread gameThread;
 
     //set olayer's default positioms
-    int playerx= 100;
+    int playerX= 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setBackground(Color.BLACK);
@@ -36,17 +42,29 @@ public class GamePanel extends  JPanel implements Runnable{
 
     public void run(){
 
+        double drawInterval = 1000000000/FPS;
         while(gameThread != null) {
-        //    System.out.println("The game loop is running");
-        //update char position
-            //draw screen the information
-        update();
-        repaint();
+            long currentTime=System.nanoTime();
+
+            update();
+            repaint();
 
         }
     }
     public void update(){
 
+        if (keyH.upPressed == true){
+            playerY -= playerSpeed;
+        }
+        else if(keyH.downPressed == true){
+            playerY += playerSpeed;
+        }
+        else if (keyH.leftPressed ==true){
+            playerX -= playerSpeed;
+        }
+        else if (keyH.rightPressed == true){
+            playerX += playerSpeed;
+        }
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
@@ -55,7 +73,7 @@ public class GamePanel extends  JPanel implements Runnable{
 
         g2.setColor(Color.WHITE);
 
-        g2.fillRect(100,100,tileSize,tileSize);
+        g2.fillRect(playerX,playerY,tileSize,tileSize);
 
         g2.dispose();
     }
