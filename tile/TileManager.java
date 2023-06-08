a18160c33277a1dd3b3a8e799301bd652f85620a
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2023-06-08 01:20:37 -0400

    Update TileManager.java

--- tile/TileManager.java
+++ tile/TileManager.java
@@ -3,6 +3,9 @@ package tile;
 import main.GamePanel;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class TileManager {
@@ -14,18 +17,33 @@ public class TileManager {
     public TileManager(GamePanel gp) {
         this.gp = gp;
 
-        tile = new Tile[10];
+        tile = new Tile[2];
 
         getTileImage();
     }
 
     public  void getTileImage(){
 
-     try{
-         tile[0] = new Tile();
-         tile[0].image = ImageIO.read(getClass().getResourceAsStream("tiles/grass.png"));
-     }catch (IOException e){
-         e.printStackTrace();
-     }
+        try{
+            File file = new File("res/tiles/grass.png");
+            FileInputStream fis = new FileInputStream(file);
+            tile[0] = new Tile();
+            tile[0].image = ImageIO.read(fis);
+
+            file = new File("res/tiles/wall.png");
+            fis = new FileInputStream(file);
+            tile[1] = new Tile();
+            tile[1].image = ImageIO.read(fis);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void draw(Graphics2D g2){
+
+        g2.drawImage(tile[0].image,0,0,gp.tileSize,gp.tileSize,null);
+        g2.drawImage(tile[1].image,48,0,gp.tileSize,gp.tileSize,null);
+        g2.drawImage(tile[0].image,96,0,gp.tileSize,gp.tileSize,null);
+
     }
 }
